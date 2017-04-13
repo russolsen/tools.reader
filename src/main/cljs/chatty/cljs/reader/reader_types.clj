e4964512e67989899b1d399781eca1493e0ead3f
Russ Olsen <russ@russolsen.com>
2017-04-13 13:52:40 -0400

    Missed namespace name.

--- src/main/cljs/chatty/cljs/reader/reader_types.clj
+++ src/main/cljs/chatty/cljs/reader/reader_types.clj
@@ -1,10 +1,10 @@
-(ns cljs.tools.reader.reader-types)
+(ns chatty.cljs.reader.reader-types)
 
 (defmacro log-source
   "If reader is a SourceLoggingPushbackReader, execute body in a source
   logging context. Otherwise, execute body, returning the result."
   [reader & body]
   `(if (and (source-logging-reader? ~reader)
-            (not (cljs.tools.reader.impl.utils/whitespace? (peek-char ~reader))))
+            (not (chatty.cljs.reader.impl.utils/whitespace? (peek-char ~reader))))
      (log-source* ~reader (^:once fn* [] ~@body))
      (do ~@body)))
